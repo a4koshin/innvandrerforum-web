bd2bd6e51b29e8afa7b74ff728a8d1a8216e62f1
a4koshin <mankajr11@gmail.com>
2025-11-30 14:39:40 +0300

    removes the default page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,7 +31,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="mx-auto max-w-7xl pt-4"> {children}</main>
+        <Navbar />
+        <main className="mx-auto max-w-7xl"> {children}</main>
       </body>
     </html>
   );
