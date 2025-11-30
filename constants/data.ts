a3921084e797f703caa5b988012430874d070886
a4koshin <mankajr11@gmail.com>
2025-11-30 12:29:07 +0300

    added web data

---
+++ constants/data.ts
@@ -0,0 +1,32 @@
+export const Navigations = [
+  {
+    id: 0,
+    name: "Hjem",
+    href: "/",
+  },
+  {
+    id: 1,
+    name: "Vårt Arbeid",
+    href: "/vårtArbeid",
+  },
+  {
+    id: 2,
+    name: "Om Oss",
+    href: "/omoss",
+  },
+  {
+    id: 3,
+    name: "Fritidsaktiviteter",
+    href: "/fritidsaktiviteter",
+  },
+  {
+    id: 4,
+    name: "Arrangementer",
+    href: "/Arrangementer",
+  },
+  {
+    id: 5,
+    name: "Kontakt",
+    href: "/Kontakt",
+  },
+];
