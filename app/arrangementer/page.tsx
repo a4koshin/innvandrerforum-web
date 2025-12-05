9a390de23d0e96db1ef4ccd61e5cc22d3b52e338
a4koshin <mankajr11@gmail.com>
2025-12-05 19:46:34 +0300

    arrangementer page is added to the web

---
+++ app/arrangementer/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ArrangementerPage = () => {
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-16 pb-24">
+      <div className="text-center mb-16">
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 to-red-600">
+            Arrangementer
+          </span>{" "}
+        </h1>
+        <p className="text-xl text-gray-600 max-w-3xl mx-auto">
+          Vi bygger broer mellom kulturer og skaper et inkluderende fellesskap
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ArrangementerPage;
