bc7388d654b94ba1c288e8c1a551a94e1899141b
a4koshin <mankajr11@gmail.com>
2025-12-13 20:42:03 +0300

    Event data is changed

--- constants/event.ts
+++ constants/event.ts
@@ -16,23 +16,24 @@ export const events: Event[] = [
   },
   {
     id: 2,
-    title: "Cabin Tour",
-    description:
-      "Explore scenic mountain cabins with guided nature walks. Experience tranquility and breathtaking views in the wilderness.",
-    image: "/fotbal1.JPG",
-  },
-  {
-    id: 3,
     title: "Bowling Championship",
     description:
       "Friendly team bowling with cosmic lights and music. Prizes for highest scores and most creative team outfits!",
+    image: "/bowling.jpeg",
+  },
+  {
+    id: 3,
+    title: "Cabin Tour",
+    description:
+      "Explore scenic mountain cabins with guided nature walks. Experience tranquility and breathtaking views in the wilderness.",
+    image: "/cabin.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Cinema Night",
+    description:
+      "Exclusive private screening of a blockbuster premiere. Enjoy gourmet popcorn, drinks, and luxury seating.",
     image: "/fotbal1.JPG",
   },
-  // {
-  //   id: 4,
-  //   title: "Cinema Night",
-  //   description:
-  //     "Exclusive private screening of a blockbuster premiere. Enjoy gourmet popcorn, drinks, and luxury seating.",
-  //   image: "/fotbal1.JPG",
-  // },
 ];
