5d138d08bf23de7f2cb4c1f903d79ee079aad964
a4koshin <mankajr11@gmail.com>
2025-12-13 10:30:56 +0300

    Annual event is added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -217,7 +217,7 @@ export default function Home() {
         </div>
 
         {/* Event Cards Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-x-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
           {events.map((event) => (
             <EventCard key={event.id} event={event} />
           ))}
