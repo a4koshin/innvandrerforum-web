58c9fb9b77fc14fc01cf8925aa82e6339e08071d
a4koshin <mankajr11@gmail.com>
2025-12-18 00:48:17 +0300

    Reasrc is adde

---
+++ constants/boards.ts
@@ -0,0 +1,13 @@
+// interface Boards {
+//   id: number;
+//   name: string;
+//   title: string;
+//   img: string;
+// }
+
+// // export const boards : Boards[] = [
+// //     {
+// //         id:0,
+// //         name ""
+// //     }
+// // ]
