8de553a261bafa65949262cbcca4f4cf8a82fc1a
a4koshin <mankajr11@gmail.com>
2025-12-18 00:19:02 +0300

    Status file is created

---
+++ constants/status.ts
@@ -0,0 +1,35 @@
+interface Image {
+  id: number;
+  image: string;
+}
+
+export const Status: Image[] = [
+  {
+    id: 0,
+    image: "/Østfold.jpeg",
+  },
+  {
+    id: 1,
+    image: "/NavFredrikstad.jpeg",
+  },
+  {
+    id: 2,
+    image: "/Sparebank1.jpeg",
+  },
+  {
+    id: 3,
+    image: "/LNUU.jpeg",
+  },
+  {
+    id: 4,
+    image: "/GjensidigeStiftelsen.jpeg",
+  },
+  {
+    id: 5,
+    image: "/FriFond.png",
+  },
+  {
+    id: 6,
+    image: "/FredrikstadKommune.png",
+  },
+];
