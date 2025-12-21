fa28fe9dae6aaa0e12ff9c204e56bf3b68136ae4
a4koshin <mankajr11@gmail.com>
2025-12-21 10:11:10 +0300

    Added width & alt elements

--- constants/status.ts
+++ constants/status.ts
@@ -1,35 +1,46 @@
 interface Image {
   id: number;
   image: string;
+  alt?: string;
+  width: string;
 }
 
 export const Status: Image[] = [
   {
     id: 0,
     image: "/Østfold.jpeg",
+    width: "",
   },
-  {
-    id: 1,
-    image: "/NavFredrikstad.jpeg",
-  },
-  {
-    id: 2,
-    image: "/Sparebank1.jpeg",
-  },
+  { id: 1, image: "/NavFredrikstad.jpeg", width: "w-40 md:w-52" }, // Increased width
+  { id: 2, image: "/Sparebank1.jpeg", width: "w-40 md:w-52" },
   {
     id: 3,
     image: "/LNUU.jpeg",
+    width: "",
   },
   {
     id: 4,
     image: "/GjensidigeStiftelsen.jpeg",
+    width: "",
   },
   {
     id: 5,
     image: "/FriFond.png",
+    width: "",
   },
   {
     id: 6,
     image: "/FredrikstadKommune.png",
+    width: "",
+  },
+  {
+    id: 7,
+    image: "/Bufdir.jpeg",
+    width: "",
+  },
+  {
+    id: 8,
+    image: "/BergensenStiftels.png",
+    width: "",
   },
 ];
