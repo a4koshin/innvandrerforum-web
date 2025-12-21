def9b0d0b344bf8a38b2948d1645a75bf13247bd
a4koshin <mankajr11@gmail.com>
2025-12-21 18:52:37 +0300

    Boards array is modified

--- constants/boards.ts
+++ constants/boards.ts
@@ -1,13 +1,45 @@
-// interface Boards {
-//   id: number;
-//   name: string;
-//   title: string;
-//   img: string;
-// }
+interface Boards {
+  id: number;
+  name: string;
+  title: string;
+  img: string;
+}
 
-// // export const boards : Boards[] = [
-// //     {
-// //         id:0,
-// //         name ""
-// //     }
-// // ]
+export const boards: Boards[] = [
+  {
+    id: 0,
+    name: "Abdi Fatah Bile",
+    title: "Aktivitetsansvarlig",
+    img: "/Abdi.jpeg",
+  },
+  {
+    id: 1,
+    name: "Ahmed Warsame",
+    title: "Daglig leder",
+    img: "/Ahmed.jpeg",
+  },
+  {
+    id: 2,
+    name: "Roble Shine",
+    title: "Styremedlem",
+    img: "/Roble.jpeg",
+  },
+  {
+    id: 3,
+    name: "Munasar Abdullah",
+    title: "Prosjektkoordinator",
+    img: "/Munasar.jpeg",
+  },
+  {
+    id: 4,
+    name: "Hassan Mohamoud",
+    title: "Styremedlem",
+    img: "/Hassan.jpeg",
+  },
+  {
+    id: 5,
+    name: "Nura Mohamed",
+    title: "Styremedlem",
+    img: "/Nura.jpeg",
+  },
+];
