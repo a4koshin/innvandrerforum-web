cd188c1bf350045ef164044f6de42ca3a568be39
a4koshin <mankajr11@gmail.com>
2025-12-21 23:13:41 +0300

    Board data is modified

--- constants/boards.ts
+++ constants/boards.ts
@@ -8,38 +8,54 @@ interface Boards {
 export const boards: Boards[] = [
   {
     id: 0,
-    name: "Abdi Fatah Bile",
-    title: "Aktivitetsansvarlig",
-    img: "/Abdi.jpeg",
-  },
-  {
-    id: 1,
-    name: "Ahmed Warsame",
-    title: "Daglig leder",
-    img: "/Ahmed.jpeg",
-  },
-  {
-    id: 2,
-    name: "Rooble Shine",
-    title: "Styremedlem",
+    name: "Hassan Mude",
+    title: "Styreleder",
     img: "/Roble.jpeg",
   },
+
   {
-    id: 3,
-    name: "Munasar Abdullah",
-    title: "Prosjektkoordinator",
-    img: "/Munasar.jpeg",
-  },
-  {
-    id: 4,
+    id: 1,
     name: "Hassan Mohamoud",
     title: "Styremedlem",
     img: "/Hassan.jpeg",
   },
   {
-    id: 5,
-    name: "Nura Mohamed",
+    id: 2,
+    name: "Nura Mohamed Ali",
     title: "Styremedlem",
     img: "/Nura.jpeg",
+  },
+  {
+    id: 3,
+    name: "Roble Shine",
+    title: "Styremedlem",
+    img: "/User.jpg",
+  },
+];
+
+export const administration: Boards[] = [
+  {
+    id: 0,
+    name: "Ahmed Warsame",
+    title: "Daglig leder",
+    img: "/Ahmed.jpeg",
+  },
+  {
+    id: 1,
+    name: "Munasar Abdullah",
+    title: "Prosjektkoordinator",
+    img: "/Munasar.jpeg",
+  },
+  {
+    id: 2,
+    name: "Abdi Fatah Bile",
+    title: "Aktivitetsansvarlig",
+    img: "/Abdi.jpeg",
+  },
+  {
+    id: 3,
+    name: "Khalid Abdulahi",
+    title: "ungdomshus",
+    img: "/User.jpg",
   },
 ];
