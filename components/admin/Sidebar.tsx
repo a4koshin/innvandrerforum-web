80e6c8c0aece4ef5a7d813424a21e8b9365ab7c6
a4koshin <mankajr11@gmail.com>
2026-01-12 20:03:25 +0300

    Sidebar is modified

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -16,18 +16,24 @@ import { LayoutDashboard, Mail, Users, LogOut } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+
 export default function AdminSidebar() {
   const pathname = usePathname();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    window.location.href = "/admin";
+  };
 
   return (
     <Sidebar>
       {/* Header */}
       <SidebarHeader>
-        <div className="flex items-center gap-2 px-2 py-3 ">
-          <div className="w-12 h-12 rounded-lg flex items-center justify-center text-white font-bold">
+        <div className="flex items-center gap-2 px-2 py-3">
+          <div className="w-12 h-12 rounded-lg flex items-center justify-center">
             <Image
               src="/logo.png"
-              alt="innvdreforum logo "
+              alt="Innvandrerforum logo"
               width={80}
               height={80}
               priority
@@ -90,7 +96,7 @@ export default function AdminSidebar() {
       <SidebarFooter>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton>
+            <SidebarMenuButton onClick={handleLogout}>
               <LogOut />
               Logout
             </SidebarMenuButton>
