ccc4614c9c62188209c6e2f12551813ff424bb52
a4koshin <mankajr11@gmail.com>
2026-01-12 12:35:18 +0300

    Layout of the app is changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import ToastProvider from "@/components/ToastProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Innvandrerforum i Østfold",
-  description:
-    "Vi arbeider for å støtte og inkludere minoritetsungdom og familier, med særlig fokus på å motvirke sosial ekskludering, ensomhet og utenforskap. Kontakt Oss",
-  icons: {
-    icon: "/logo.svg",
-  },
-};
+import "@/app/globals.css";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <main className=""> {children}</main>
-        <Footer />
-        {/* Client-only toaster */}
-        <ToastProvider />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
