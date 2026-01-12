20c01e3020bc340ab0b8f0698c5b99013747d444
a4koshin <mankajr11@gmail.com>
2026-01-12 11:57:20 +0300

    Helper is added

---
+++ helper/fetchError.ts
@@ -0,0 +1,10 @@
+export async function getErrorMessage(res: Response): Promise<string> {
+  try {
+    const data = await res.json();
+    console.log(data.message, "from hlpeer");
+
+    return data?.message || "Something went wrong";
+  } catch {
+    return "Something went wrong";
+  }
+}
