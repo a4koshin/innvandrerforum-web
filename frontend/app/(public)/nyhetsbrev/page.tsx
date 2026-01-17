0ca4c05f8e00270a7861c9cda3563728799ada33
a4koshin <mankajr11@gmail.com>
2026-01-17 13:35:46 +0300

    feat: add nodemon for development and update page component with a development notice

--- frontend/app/(public)/nyhetsbrev/page.tsx
+++ frontend/app/(public)/nyhetsbrev/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className=" mt-32 mx-auto flex justify-center items-center text-2xl font-bold relative mb-32">
+      <div className="bg-red-100 rounded-md  px-4 py-6 text-blue-900">
+        🚧This feature is currently under development. Please check back
+        soon.🚧
+      </div>
+    </div>
+  );
 };
 
 export default page;
