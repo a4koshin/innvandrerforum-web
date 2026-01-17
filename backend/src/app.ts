735d6974738ae7f1040beaf9b6a14b613dff8806
a4koshin <mankajr11@gmail.com>
2026-01-17 14:11:34 +0300

    refactor: update backend dependencies and improve server setup
    - Changed nodemon command in package.json for better TypeScript support.
    - Updated @prisma/client and prisma versions to 5.22.0.
    - Downgraded express to version 4.18.2 for compatibility.
    - Added dotenv and cors middleware in app.ts for environment variable management and CORS support.
    - Refactored Prisma client export in client.ts to use default export.
    - Minor adjustments in tsconfig.json for clarity.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,2 +1,23 @@
 import express from "express";
-import 
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 5001;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+//Routes
+
+// server start
+app.listen(port, () => {
+  console.log("Server is running on port ", port);
+});
